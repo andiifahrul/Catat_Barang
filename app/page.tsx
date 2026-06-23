884cb3666dcc1a2fcea5b10d338775905357d990
andiifahrul <andiahmadfahrul@gmail.com>
2026-06-23 19:07:46 +0800

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import { redirect } from "next/navigation"; // [cite: 226]
+import { redirect } from "next/navigation";
 
 export default function RootPage() {
-  // Langsung alihkan rute utama ke halaman dashboard [cite: 227]
-  redirect("/dashboard"); // [cite: 227]
-} 
+  redirect("/dashboard");
+}
